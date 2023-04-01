a678b3404f57bd28256130f9868f20a5f1891225
kimjunbo <ns9337@naver.com>
2023-04-01 15:29:57 +0900

    refact : naver code convention 적용 (패키지명 _없이 소문자로만 구성, System.properties에서 getAt()이 아닌 getProperty() 사용)

--- app/src/main/java/com/atticket/app/App.java
+++ app/src/main/java/com/atticket/app/App.java
@@ -1,13 +1,11 @@
-package at_ticket.app;
+package com.atticket.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
-
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
