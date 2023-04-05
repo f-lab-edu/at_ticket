95c65ee934f7b1456eacc10cd5d505ed537ab6f5
kimjunbo <ns9337@naver.com>
2023-04-05 17:03:05 +0900

    feat : ReservationController 뼈대 추가

---
+++ reservation/src/main/java/com/atticket/dto/SampleDto.java
@@ -0,0 +1,12 @@
+package com.atticket.dto;
+
+import lombok.Getter;
+
+@Getter
+public class SampleDto {
+	private String content;
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
