75fa4b0ec0c358f5310da2836481a230c18719f4
kimjunbo <ns9337@naver.com>
2023-04-05 16:06:46 +0900

    refact : 모듈(reservation,payment) 분리 및 공통 그래들 적용

---
+++ payment/src/main/java/com/atticket/PaymentApp.java
@@ -0,0 +1,11 @@
+package com.atticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentApp {
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentApp.class, args);
+	}
+}
--- reservation/src/main/java/com/atticket/ReservationApp.java
+++ reservation/src/main/java/com/atticket/ReservationApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
+public class ReservationApp {
 	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
+		SpringApplication.run(ReservationApp.class, args);
 	}
 }
