951f46399549ef6a1b341defbe0a3e75c216e41a
kimjunbo <ns9337@naver.com>
2023-04-06 22:46:25 +0900

    chore : naver formatter 적용

--- reservation/src/main/java/com/atticket/controller/ReservationController.java
+++ reservation/src/main/java/com/atticket/controller/ReservationController.java
@@ -1,10 +1,11 @@
 package com.atticket.controller;
 
-import com.atticket.dto.SampleDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.atticket.dto.SampleDto;
 
 @RestController
 @RequestMapping("/reservation")
