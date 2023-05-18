917e88fdbd69cb55fb23e0116c4c8d9178c07cb3
gaya <qltkd8487@gmail.com>
2023-05-18 22:43:49 +0900

    모듈 분리  및 모듈 간 통신( feign client 설정) (#15)

---
+++ product/src/main/java/com/atticket/product/client/dto/GetReservationSeatsResDto.java
@@ -0,0 +1,44 @@
+package com.atticket.product.client.dto;
+
+import java.beans.ConstructorProperties;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class GetReservationSeatsResDto {
+
+	public final List<ReservedSeatsDto> reservedSeats;
+
+	@ConstructorProperties({"reservedSeats"})
+	public GetReservationSeatsResDto(List<ReservedSeatsDto> reservedSeats) {
+		this.reservedSeats = reservedSeats;
+	}
+
+	//예약된 좌석 리스트
+
+	@Getter
+	@ToString
+	public static class ReservedSeatsDto {
+
+		//예약좌석 id
+		private final Long id;
+
+		//공연 id
+		private final Long showId;
+
+		//좌석 id
+		private final Long seatId;
+
+		@ConstructorProperties({"id", "showId", "seatId"})
+		public ReservedSeatsDto(Long id, Long showId, Long seatId) {
+			this.id = id;
+			this.showId = showId;
+			this.seatId = seatId;
+		}
+
+	}
+
+}
--- product/src/main/java/com/atticket/product/service/ReservedSeatService.java
+++ product/src/main/java/com/atticket/product/service/ReservedSeatService.java
@@ -1,18 +1,20 @@
 package com.atticket.product.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.atticket.product.client.client.ReservationFeignClient;
 import com.atticket.product.domain.ReservedSeat;
-import com.atticket.product.repository.ReservedSeatRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class ReservedSeatService {
-	private final ReservedSeatRepository reservedSeatRepository;
+
+	private final ReservationFeignClient reservationFeignClient;
 
 	/**
 	 * 공연의 예매된 좌석 리스트 조회
@@ -20,7 +22,22 @@ public class ReservedSeatService {
 	 * @return
 	 */
 	public List<ReservedSeat> getReservedSeatsByShowId(Long showId) {
-		return reservedSeatRepository.findShowSeatByShowId(showId);
+		
+		List<ReservedSeat> reservedSeats = reservationFeignClient.getReservationSeats(showId)
+			.getData()
+			.getReservedSeats()
+			.stream()
+			.map(
+				seat ->
+					ReservedSeat.builder()
+						.id(seat.getId())
+						.seatId(seat.getSeatId())
+						.showId(seat.getShowId())
+						.build()
+			)
+			.collect(Collectors.toList());
+
+		return reservedSeats;
 	}
 
 }
