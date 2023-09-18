1554f97a2e4379a32644b549219c9897e8d86310
HyungInYang <qltkd8487@gmail.com>
2023-09-18 22:23:51 +0900

    Restdoc 테스트 코드 작성  * 공연별 남은 좌석 조회

--- product/src/test/java/com/atticket/product/controller/ProductControllerTest.java
+++ product/src/test/java/com/atticket/product/controller/ProductControllerTest.java
@@ -1,62 +1,170 @@
 package com.atticket.product.controller;
 
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
-import com.atticket.common.response.BaseResponse;
 import com.atticket.product.domain.Grade;
 import com.atticket.product.domain.Place;
 import com.atticket.product.domain.Product;
-import com.atticket.product.dto.response.GetProductResDto;
+import com.atticket.product.repository.HallRepository;
+import com.atticket.product.repository.SeatRepository;
+import com.atticket.product.repository.ShowRepository;
+import com.atticket.product.repository.ShowSeatRepository;
+import com.atticket.product.repository.WishProductRepository;
 import com.atticket.product.service.GradeService;
 import com.atticket.product.service.ProductService;
+import com.atticket.product.service.ReservedSeatService;
 import com.atticket.product.service.ShowService;
+import com.atticket.product.type.AgeLimit;
+import com.atticket.product.type.Category;
+import com.atticket.product.type.Region;
+import com.atticket.product.type.SubCategory;
 
+@AutoConfigureRestDocs
+@WebMvcTest(ProductController.class)
 public class ProductControllerTest {
 
-	private ProductService productService = mock(ProductService.class);
-	private ShowService showService = mock(ShowService.class);
-	private GradeService gradeService = mock(GradeService.class);
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private ProductService productService;
+
+	@MockBean
+	private ShowService showService;
+	@MockBean
+	private GradeService gradeService;
+
+	@MockBean
+	private ReservedSeatService reservedSeatService;
+
+	@MockBean
+	private ShowRepository showRepository;
+
+	@MockBean
+	private SeatRepository seatRepository;
+
+	@MockBean
+	private ShowSeatRepository showSeatRepository;
+
+	@MockBean
+	private WishProductRepository wishProductRepository;
+
+	@MockBean
+	private HallRepository hallRepository;
+
+	@MockBean
 	private ProductController productController;
 
-	@BeforeEach
-	public void setUpTest() {
-		productController = new ProductController(productService, showService, gradeService);
-	}
+	@MockBean
+	private KafkaTemplate kafkaTemplate;
 
 	@Test
 	@DisplayName("상품 상세 조회 테스트")
-	void getProductTest() {
+	void getProductTest() throws Exception {
 
-		//Given
+		// Create a ProductDto for testing
+
 		Long productId = 1L;
 
-		Product givenProduct = Product.builder()
-			.name("테스트")
-			.place(Place.builder().build())
-			.build();
-		List<Grade> givenGrades = Arrays.asList(Grade.builder().build());
-		List<LocalDate> givenLocalDates = Arrays.asList(LocalDate.of(2023, 3, 1));
+		// Create a LocalDate list for testing
+		List<LocalDate> showDatesList = showDatesList = Arrays.asList(
 
-		//When
-		when(productService.getProductById(productId)).thenReturn(givenProduct);
-		when(gradeService.getGradesByProductId(productId)).thenReturn(givenGrades);
-		when(showService.getShowDatesByProductId(productId)).thenReturn(givenLocalDates);
+		);
 
-		BaseResponse<GetProductResDto> result = productController.getProduct(productId);
+		// Mock the service calls
+		when(productService.getProductById(productId)).thenReturn(
+			Product.builder()
+				.id(1L)
+				.name("캣츠")
+				.explains("공연설명")
+				.category(Category.MUSICAL)
+				.subCategory(SubCategory.ORIGINAL)
+				.runningTime(LocalTime.of(02, 00, 00))
+				.interMission(LocalTime.of(20, 00, 00))
+				.startDate(LocalDate.of(2023, 3, 1))
+				.endDate(LocalDate.of(2024, 06, 30))
+				.image("http://image.img")
+				.ageLimit(AgeLimit.TWELVE)
+				.place(
+					Place.builder()
+						.id(1L)
+						.name("서울 공연장1")
+						.address("서울 어쩌구구 저쩌구동")
+						.phoneNumber("010 123 1234")
+						.region(Region.SEOUL)
+						.build()
+				).build()
+		);
+		when(gradeService.getGradesByProductId(productId)).thenReturn(
+			Arrays.asList(Grade.builder().id(1L).type("A").price(1000).build())
+		);
+		when(showService.getShowDatesByProductId(anyLong())).thenReturn(
+			Arrays.asList(
+				LocalDate.of(2023, 3, 1),
+				LocalDate.of(2023, 3, 2)
+			)
+		);
+		//
+		// GetProductResDto getProductResDto = new GetProductResDto(
+		//
+		// 	GetProductResDto.ProductDto.builder()
+		// 		.name("캣츠")
+		// 		.explain("공연설명")
+		// 		.category(Category.MUSICAL)
+		// 		.subCategory(SubCategory.ORIGINAL)
+		// 		.runningTime(LocalTime.of(02, 00, 00))
+		// 		.startDate(LocalDate.of(2023, 3, 1))
+		// 		.endDate(LocalDate.of(2024, 06, 30))
+		// 		.image("http://image.img")
+		// 		.ageLimit(AgeLimit.TWELVE)
+		// 		.build(),
+		// 	Arrays.asList(
+		// 		GetProductResDto.GradeDto.builder()
+		// 			.type("A")
+		// 			.price(1000)
+		// 			.build()
+		// 	),
+		// 	Arrays.asList(
+		// 		LocalDate.of(2023, 3, 1),
+		// 		LocalDate.of(2023, 3, 2)
+		// 	)
+		// );
+		//
+		// given(GetProductResDto.construct(
+		// 	productService.getProductById(productId)
+		// 	, gradeService.getGradesByProductId(productId)
+		// 	, showService.getShowDatesByProductId(productId)))
+		// 	.willReturn(getProductResDto);
 
-		//Then
-		Assertions.assertEquals(result.getData().getProduct().getName(), givenProduct.getName());
+		ResultActions result = this.mockMvc.perform(
+			RestDocumentationRequestBuilders.get("/products/{productId}", productId)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)
+		);
+
+		//then
+		result.andExpect(status().isOk())
+			.andDo(MockMvcResultHandlers.print());
 
 	}
-
 }
--- product/src/test/java/com/atticket/product/controller/ShowControllerTest.java
+++ product/src/test/java/com/atticket/product/controller/ShowControllerTest.java
@@ -2,47 +2,98 @@ package com.atticket.product.controller;
 
 import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.Arrays;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.atticket.product.dto.service.GetRemainSeatCntSvcDto;
+import com.atticket.product.repository.HallRepository;
+import com.atticket.product.repository.ShowRepository;
+import com.atticket.product.repository.ShowSeatRepository;
+import com.atticket.product.repository.WishProductRepository;
+import com.atticket.product.service.GradeService;
+import com.atticket.product.service.ProductService;
+import com.atticket.product.service.ReservedSeatService;
+import com.atticket.product.service.SeatService;
 import com.atticket.product.service.ShowSeatService;
 import com.atticket.product.service.ShowService;
+import com.atticket.product.service.WishProductService;
 
-public class ShowControllerTest {
+@AutoConfigureRestDocs
+@WebMvcTest(ShowControllerTest.class)
+class ShowControllerTest {
 
-	private ShowController showController;
+	@Autowired
+	private MockMvc mockMvc;
 
-	private ShowSeatService showSeatService = mock(ShowSeatService.class);
+	@MockBean
+	private ShowSeatService showSeatService;
 
-	@BeforeEach
-	public void setUpTest(){
+	@MockBean
+	private ShowService showService;
 
-		showController = new ShowController(showSeatService);
-	}
+	@MockBean
+	private ProductService productService;
 
+	@MockBean
+	private GradeService gradeService;
+
+	@MockBean
+	private SeatService seatService;
+
+	@MockBean
+	private ReservedSeatService reservedSeatService;
+
+	// @MockBean
+	private WishProductService wishProductService;
+
+	@MockBean
+	private ShowRepository showRepository;
+
+	@MockBean
+	private ShowSeatRepository showSeatRepository;
+
+	@MockBean
+	private HallRepository hallRepository;
+
+	@MockBean
+	private KafkaTemplate kafkaTemplate;
+
+	@MockBean
+	private WishProductRepository wishProductRepository;
 
 	@Test
 	@DisplayName("공연의 남은 좌석수 조회 테스트")
 	void getShowsTest() throws Exception {
 
+		Long showId = 1L;
+
 		//given
-		//자리 - 등급 매핑 정보
-		given(showSeatService.getRemainSeatCntByShowId(1L)).willReturn(
+		given(showSeatService.getRemainSeatCntByShowId(showId)).willReturn(
 			Arrays.asList(
 				GetRemainSeatCntSvcDto.builder()
 					.showId(1L)
@@ -59,14 +110,42 @@ public class ShowControllerTest {
 			)
 		);
 
-		Long showId = 1L;
+		ResultActions result = this.mockMvc.perform(
+			RestDocumentationRequestBuilders.get("/shows/{showId}/seats/count", showId)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)
+		);
 
-//		mockMvc.perform(get("/shows/" + showId + "/seats/count"))
-//			.andExpect(status().isOk())
-//			.andExpect(MockMvcResultMatchers.jsonPath("$.data.remainSeats", notNullValue()))
-//			.andDo(MockMvcResultHandlers.print());
+		//then
+		result.andExpect(status().isOk())
+			.andExpect(MockMvcResultMatchers.jsonPath("$.data.remainSeats", notNullValue()))
+			.andDo(MockMvcResultHandlers.print())
+			.andDo(document("getRemainSeatsCnt",
+				//Json 포매팅
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				pathParameters(
+					parameterWithName("showId").description("공연 ID")
+				),
+				responseFields(
+					fieldWithPath("code").type(JsonFieldType.NUMBER).description("결과코드"),
+					fieldWithPath("message").type(JsonFieldType.STRING).description("결과 메세지"),
+					fieldWithPath("data.remainSeats[].showId")
+						.type(JsonFieldType.NUMBER)
+						.description("공연 ID"),
+					fieldWithPath("data.remainSeats[].gradeId")
+						.type(JsonFieldType.NUMBER)
+						.description("등급 ID"),
+					fieldWithPath("data.remainSeats[].gradeNm")
+						.type(JsonFieldType.STRING)
+						.description("공연 이름"),
+					fieldWithPath("data.remainSeats[].seatCnt")
+						.type(JsonFieldType.NUMBER)
+						.description("남은 좌석수")
+				)
+			));
 
-	//	verify(showSeatService).getRemainSeatCntByShowId(1L);
+		verify(showSeatService).getRemainSeatCntByShowId(1L);
 
 	}
 }
