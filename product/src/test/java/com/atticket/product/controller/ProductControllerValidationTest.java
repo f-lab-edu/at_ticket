e98b71d2370acbb0958ca44e786ce441fa6694be
gaya <qltkd8487@gmail.com>
2023-09-21 23:01:36 +0900

    Rest Doc dependency 추가 (#35)

--- product/src/test/java/com/atticket/product/controller/ProductControllerValidationTest.java
+++ product/src/test/java/com/atticket/product/controller/ProductControllerValidationTest.java
@@ -11,11 +11,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.atticket.product.repository.HallRepository;
+import com.atticket.product.repository.ShowRepository;
+import com.atticket.product.repository.ShowSeatRepository;
+import com.atticket.product.repository.WishProductRepository;
 import com.atticket.product.service.GradeService;
 import com.atticket.product.service.ProductService;
+import com.atticket.product.service.ReservedSeatService;
+import com.atticket.product.service.SeatService;
+import com.atticket.product.service.ShowSeatService;
 import com.atticket.product.service.ShowService;
+import com.atticket.product.service.WishProductService;
 
 @WebMvcTest(ProductController.class)
 public class ProductControllerValidationTest {
@@ -24,45 +33,72 @@ public class ProductControllerValidationTest {
 	private MockMvc mockMvc;
 
 	@MockBean
-	ProductService productService;
+	private ShowSeatService showSeatService;
 
 	@MockBean
-	ShowService showService;
+	private ShowService showService;
 
 	@MockBean
-	GradeService gradeService;
+	private ProductService productService;
 
-//	@Test
-//	@DisplayName("없는 상품 상세 조회 테스트")
-//	void getProductNothingTest() throws Exception {
-//
-//		//given
-//		given(productService.getProductById(11L)).willReturn(null);
-//
-//		String productId = "11";
-//
-//		mockMvc.perform(get("/products/" + productId))
-//			.andExpect(status().isOk())
-//			.andExpect(jsonPath("$.message").value("조회된 데이터가 없습니다."))
-//			.andDo(print());
-//
-//	}
-//
-//	@Test
-//	@DisplayName("상품 조회 validation 테스트")
-//	void getProductValidationTest() throws Exception {
-//
-//		//given
-//		given(productService.getProductById(11L)).willReturn(null);
-//
-//		String wrongId = "product";
-//
-//		mockMvc.perform(get("/products/" + wrongId))
-//			.andExpect(status().isOk())
-//			.andExpect(jsonPath("$.code").value("400"))
-//			.andExpect(jsonPath("$.message").value("validation 에러입니다."))
-//			.andDo(print());
-//
-//	}
+	@MockBean
+	private GradeService gradeService;
+
+	@MockBean
+	private SeatService seatService;
+
+	@MockBean
+	private ReservedSeatService reservedSeatService;
+
+	@MockBean
+	private WishProductService wishProductService;
+
+	@MockBean
+	private ShowRepository showRepository;
+
+	@MockBean
+	private ShowSeatRepository showSeatRepository;
+
+	@MockBean
+	private HallRepository hallRepository;
+
+	@MockBean
+	private WishProductRepository wishProductRepository;
+
+	@MockBean
+	private KafkaTemplate kafkaTemplate;
+
+	@Test
+	@DisplayName("없는 상품 상세 조회 테스트")
+	void getProductNothingTest() throws Exception {
+
+		//given
+		given(productService.getProductById(11L)).willReturn(null);
+
+		Long productId = 1L;
+
+		this.mockMvc.perform(get("/products/{productId}", productId))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.message").value("조회된 데이터가 없습니다."))
+			.andDo(print());
+
+	}
+
+	@Test
+	@DisplayName("상품 조회 validation 테스트")
+	void getProductValidationTest() throws Exception {
+
+		//given
+		given(productService.getProductById(11L)).willReturn(null);
+
+		String wrongId = "product";
+
+		this.mockMvc.perform(get("/products/" + wrongId))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.code").value("400"))
+			.andExpect(jsonPath("$.message").value("validation 에러입니다."))
+			.andDo(print());
+
+	}
 
 }
