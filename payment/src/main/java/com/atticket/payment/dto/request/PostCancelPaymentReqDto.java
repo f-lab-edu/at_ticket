2cba8c668326e5a698f2babed46cd0bdb2ec483d
HyungInYang <qltkd8487@gmail.com>
2023-09-24 22:09:12 +0900

    Restdoc 테스트 코드 작성  * Payment

--- payment/src/main/java/com/atticket/payment/dto/request/PostCancelPaymentReqDto.java
+++ payment/src/main/java/com/atticket/payment/dto/request/PostCancelPaymentReqDto.java
@@ -6,51 +6,55 @@ import org.springframework.validation.annotation.Validated;
 
 import com.atticket.payment.type.LinkedPlatformType;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @ToString
 @Getter
 @Validated
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
 public class PostCancelPaymentReqDto {
 
 	//결제 번호
 	@NotNull
-	private String paymentId;
+	private final String paymentId;
 
 	//주문 번호
 	@NotNull
-	private String orderId;
+	private final String orderId;
 
 	//취소 금액
-	private Double amount;
+	private final Double amount;
 
 	//취소요청금액 중 면세금액
-	private Double taxFree;
+	private final Double taxFree;
 
 	//취소 사유
-	private String reason;
+	private final String reason;
 
 	//환불계좌 예금주
-	private String refundHolder;
+	private final String refundHolder;
 
 	// 환불계좌 예금주 연락처
-	private String refundTel;
+	private final String refundTel;
 
 	//환불계좌 은행
-	private String refundBank;
+	private final String refundBank;
 
 	//환불계좌 계좌번호
-	private String refundAccount;
+	private final String refundAccount;
 
 	//결제 연동 타입
 	@NotNull
-	private LinkedPlatformType linkedPlatform;
+	private final LinkedPlatformType linkedPlatform;
 
 	//(아임포트 판매자) API 키
-	private String impKey;
+	private final String impKey;
 
 	//(아임포트 판매자) 비밀 키
-	private String impSecret;
+	private final String impSecret;
 
 }
